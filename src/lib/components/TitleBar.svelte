2d2b648fbb0a0d9cf3f5390cff1ea57e556c823c
Evan Rose <evanandrewrose@gmail.com>
2025-07-20 20:08:14 -0400

    Add cwal.gg to title bar

--- src/lib/components/TitleBar.svelte
+++ src/lib/components/TitleBar.svelte
@@ -10,34 +10,40 @@
 
 <div class="z-10">
   <Menubar.Root
-    class="flex justify-end bg-clip-border rounded-b-none"
+    class="flex justify-between items-center bg-clip-border rounded-b-none"
     data-tauri-drag-region
   >
-    <Menubar.Menu>
-      <Button
-        onclick={() => window.minimize()}
-        variant="ghost"
-        size="icon"
-        class="size-8"
-      >
-        <MinusIcon />
-      </Button>
-      <Button
-        onclick={() => window.toggleMaximize()}
-        variant="ghost"
-        size="icon"
-        class="size-8"
-      >
-        <SquareIcon />
-      </Button>
-      <Button
-        onclick={() => window.close()}
-        variant="ghost"
-        size="icon"
-        class="size-8"
-      >
-        <XIcon />
-      </Button>
-    </Menubar.Menu>
+    <div class="flex-1"></div>
+    <div class="flex-1 flex justify-center">
+      <span class="text-sm font-medium text-muted-foreground">cwal.gg</span>
+    </div>
+    <div class="flex-1 flex justify-end">
+      <Menubar.Menu>
+        <Button
+          onclick={() => window.minimize()}
+          variant="ghost"
+          size="icon"
+          class="size-8"
+        >
+          <MinusIcon />
+        </Button>
+        <Button
+          onclick={() => window.toggleMaximize()}
+          variant="ghost"
+          size="icon"
+          class="size-8"
+        >
+          <SquareIcon />
+        </Button>
+        <Button
+          onclick={() => window.close()}
+          variant="ghost"
+          size="icon"
+          class="size-8"
+        >
+          <XIcon />
+        </Button>
+      </Menubar.Menu>
+    </div>
   </Menubar.Root>
 </div>
